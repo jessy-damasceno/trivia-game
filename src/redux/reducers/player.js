3486887699e412478ff319d64f7ea2cd1bffaeef
Jessy Damasceno <jessy_bas@hotmail.com>
2022-06-28 16:22:58 -0300

    add initial structure to project (Redux)

---
+++ src/redux/reducers/player.js
@@ -0,0 +1,13 @@
+const INITIAL_STATE = {
+  name: '', // nome-da-pessoa
+  assertions: 0, // número-de-acertos
+  score: 0, // pontuação
+  gravatarEmail: '', // email-da-pessoa
+};
+
+export default player = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
