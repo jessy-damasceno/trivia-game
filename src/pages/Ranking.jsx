7d108e4e5cde5bd256b62da61b4994d9803318d6
Felipe Moreira da Cruz <piradous@gmail.com>
2022-07-01 16:43:09 -0300

    requisito 17e18

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,9 +1,44 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
 export default class Ranking extends React.Component {
+  onClick = () => {
+    const { history } = this.props;
+    history.push('/');
+  };
+
+  playerRanking = () => {
+    const ranking = JSON.parse(localStorage.getItem('ranking'));
+    return (ranking.map((player) => (
+      <div key={ player.name }>
+        <p>{player.name}</p>
+        <p>{player.score}</p>
+        <img src={ player.picture } alt={ player.name } />
+      </div>
+    )));
+  }
+
   render() {
     return (
-      <h1 data-testid="ranking-title">Ranking</h1>
+      <>
+        <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          type="button"
+          onClick={ this.onClick }
+          data-testid="btn-go-home"
+        >
+          Voltar ao Início
+        </button>
+        {this.playerRanking}
+      </>
     );
   }
 }
+
+Ranking.propTypes = {
+  // assertions: PropTypes.number.isRequired,
+  // score: PropTypes.number.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
